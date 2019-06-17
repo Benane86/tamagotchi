ed4b325324bbd7b525b83d6d7a90fa96e7255c32
Benane86 <47816453+Benane86@users.noreply.github.com>
2019-06-17 15:29:30 +0200

    Update Program.cs

--- tamagotchi/Program.cs
+++ tamagotchi/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using System.Media;
 
 namespace tamagotchi
 {
@@ -10,6 +12,53 @@ namespace tamagotchi
     {
         static void Main(string[] args)
         {
+            int status = 0;
+            do
+            {
+                Console.WriteLine("(.)");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("(:)");
+                Thread.Sleep(1000);
+                Console.Clear();
+                status = status + 1;
+            } while (status < 10);
+
+            int blitz = 10;
+            do
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Thread.Sleep(100);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Thread.Sleep(100);
+            } while (blitz < 10);
+
+            Console.WriteLine("Das Tamagotchi ist ausgeschlüpft!");
+            Console.ReadKey();
+            do
+            {
+            Console.WriteLine("(*-*)");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine(" (*o*)");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("  (*-*)");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("   (*o*)");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("  (*-*)");
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine(" (*o*)");
+                Thread.Sleep(1000);
+                Console.Clear();
+                status = status + 1;
+        } while (status < 20);
+            Console.ReadKey();
+
         }
     }
 }
